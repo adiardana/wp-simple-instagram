700a4dde98b6d88e94d1988d3ae1451774ed01ce
Adi Ardana <adhixz@gmail.com>
2016-10-28 23:19:07 +0800

    add admin shortcode details

--- includes/admin-shortcode.php
+++ includes/admin-shortcode.php
@@ -1,19 +1,39 @@
-<div id="sig-wrap" class="wrap">
+<div class="wrap">
     <h2><?php _e('Shortcode Options'); ?></h2>
-    
-    <table class="form-table">
+
+    <h3>Shortcode : [sig count="5" size="thumbnail" class="ig" heading="Your Heading"]</h3>
+    <table class="shortcode-table">
         <thead>
             <tr>
-                <th>Shortcode</th>
+                <th>Parameter</th>
                 <th>Description</th>
             </tr>
         </thead>
         <tbody>
             <tr>
-                <td>id</td>
-                <td>[sig id="YOUR_ID"]</td>
+                <td>count</td>
+                <td>number of media to show</td>
+            </tr>
+            <tr>
+                <td>class</td>
+                <td>add custom class before the gallery</td>
+            </tr>
+
+            <tr>
+                <td>size</td>
+                <td>size of the image, there only 3 available options<br/>
+                thumbnail = 150x150<br/>
+                low_resolution = 320x320<br/>
+                standard_resolution = 640x640<br/>
+                </td>
+            </tr>
+            <tr>
+                <td>heading</td>
+                <td>Heading of the gallery (not mandatory)</td>
             </tr>
         </tbody>
     </table>
-
+    <hr>
+    <h2>Issue or Feature Request</h2>
+    <p>Click <a href="mailto:adhixz@gmail.com" target="_blank">here</a> if you found an issue or if you have an idea for the plugin.</p>
 </div>
